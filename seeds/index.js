1cd05a42b97c85fbbddb3e8f93c9e188894adb77
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-06-03 12:24:13 +0200

    feat: add a seed file to seed the db

---
+++ seeds/index.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const Campground = require('../models/campground')
+const cities = require('./cities')
+const { descriptors, places } = require('./seedHelpers')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+
+const dbCon = mongoose.connection
+dbCon.on('error', console.error.bind(console, 'connection error: '))
+dbCon.once('open', () => {
+    console.log('Database connected')
+})
+
+const sample = arr => arr[Math.floor(Math.random() * arr.length)]
+
+const seedDB = async () => {
+    await Campground.deleteMany({})
+    for (let i = 0; i < 50; i++) {
+        const randomNum = Math.floor(1000 * Math.random())
+        const camp = new Campground({
+            title: `${sample(descriptors)} ${sample(places)}`,
+            location: `${cities[randomNum].city}, ${cities[randomNum].state}`
+        })
+        await camp.save()
+    }
+}
+
+seedDB()
+    .then(() => {
+        dbCon.close()
+    })
