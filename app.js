63f10a4838399d124269473a7527ca1ce727a312
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-06-04 10:30:30 +0200

    feat: add DELETE functionality

--- app.js
+++ app.js
@@ -58,6 +58,12 @@ app.put('/campgrounds/:id', async (req, res) => {
     res.redirect(`/campgrounds/${id}`)
 })
 
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params
+    await Campground.findByIdAndDelete(id)
+    res.redirect(`/campgrounds`)
+})
+
 app.listen(port, () => {
     console.log(`Serving on port ${port}`)
 })
