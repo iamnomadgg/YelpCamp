111ae8541c699ff08708a72aa5633ba60cf3a927
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-06-04 17:54:14 +0200

    feat: add ejs-mate as ejs engine

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const ejsMate = require('ejs-mate')
 const Campground = require('./models/campground')
 const methodOverride = require('method-override')
 
@@ -15,6 +16,7 @@ dbCon.once('open', () => {
 const app = express()
 const port = 3000
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
