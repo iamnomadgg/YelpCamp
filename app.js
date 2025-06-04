43e4aa6adf6e28d820e33524592e5eab9561cdd1
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-06-04 10:10:34 +0200

    feat: create a PUT request

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const Campground = require('./models/campground')
+const methodOverride = require('method-override')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -18,6 +19,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.redirect('/campgrounds')
@@ -48,6 +50,10 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findById(id)
     res.render('campgrounds/edit', { campground })
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    res.send('PUT REQUEST')
 })
 
 app.listen(port, () => {
