3c038cb2f166eb059099d1628542b0852ce731b1
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-06-11 10:32:25 +0200

    feat: configure passport modules

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const flash = require('connect-flash')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./models/user')
 
 const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews')
@@ -41,11 +44,23 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig))
 app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success')
     res.locals.error = req.flash('error')
     next()
+})
+
+app.get('/fakeuser', async (req, res) => {
+    const user = new User({ email: 'abc@gmail.com', username: 'abc' })
+    const newUser = await User.register(user, '123')
+    res.send(newUser)
 })
 
 app.use('/campgrounds', campgrounds)
