90f9161de8761557aad570b7f03102d443f2e73c
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-06-12 19:37:01 +0200

    refactor: move isAuthor method into middleware file

--- middleware.js
+++ middleware.js
@@ -1,3 +1,5 @@
+const Campground = require('./models/campground')
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.flash('error', 'you must be signed in first')
@@ -11,4 +13,14 @@ module.exports.moveLastPageInfoToLocals = (req, res, next) => {
         res.locals.lastPageInfo = req.session.lastPageInfo;
     }
     next();
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+    const { id } = req.params
+    const campground = await Campground.findById(id)
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that')
+        return res.redirect(`/campgrounds/${id}`)
+    }
+    next()
 }
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,8 +3,7 @@ const router = express.Router()
 const ExpressError = require('../utils/ExpressError')
 const Campground = require('../models/campground')
 const { campgroundSchema } = require('../schemas.js')
-const { isLoggedIn } = require('../middleware.js')
-
+const { isLoggedIn, isAuthor } = require('../middleware.js')
 
 const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body)
@@ -18,15 +17,7 @@ const validateCampground = (req, res, next) => {
     }
 }
 
-const isAuthor = async (req, res, next) => {
-    const { id } = req.params
-    const campground = await Campground.findById(id)
-    if (!campground.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that')
-        return res.redirect(`/campgrounds/${id}`)
-    }
-    next()
-}
+
 
 router.get('/', async (req, res) => {
     const campgrounds = await Campground.find({})
