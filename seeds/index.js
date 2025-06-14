74b704c04a36698832fe12344f5c530b4acf51f2
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-06-14 19:01:09 +0200

    ops: change image format in seed file

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,23 @@ const seedDB = async () => {
             author: '684a948b691d36f93d26a955',
             title: `${sample(descriptors)} ${sample(places)}`,
             location: `${cities[randomNum].city}, ${cities[randomNum].state}`,
-            image: `https://picsum.photos/400?random=${Math.random()}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quo accusamus quam fuga consectetur, neque praesentium ipsam. Corrupti perferendis facilis temporibus esse! Maxime, laudantium. Consequatur ab dicta eius labore tenetur.',
-            price
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dsmhaw698/image/upload/v1749919780/YelpCamp/hbgdco25o4djv765qb70.jpg',
+                    filename: 'YelpCamp/hbgdco25o4djv765qb70',
+                },
+                {
+                    url: 'https://res.cloudinary.com/dsmhaw698/image/upload/v1749919782/YelpCamp/hl0jhzjubrjtupgdfr71.jpg',
+                    filename: 'YelpCamp/hl0jhzjubrjtupgdfr71',
+                },
+                {
+                    url: 'https://res.cloudinary.com/dsmhaw698/image/upload/v1749919783/YelpCamp/mrjdtbdq5idfjykknmbf.jpg',
+                    filename: 'YelpCamp/mrjdtbdq5idfjykknmbf',
+                }
+            ],
+
         })
         await camp.save()
     }
