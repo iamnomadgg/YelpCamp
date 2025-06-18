d3a984baa685b5dcaa63d513e97e779eba5beafd
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-06-18 12:30:18 +0200

    chore: increase generated campground number in seeds file

--- seeds/index.js
+++ seeds/index.js
@@ -15,7 +15,7 @@ const sample = arr => arr[Math.floor(Math.random() * arr.length)]
 
 const seedDB = async () => {
     await Campground.deleteMany({})
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 300; i++) {
         const randomNum = Math.floor(1000 * Math.random())
         const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
