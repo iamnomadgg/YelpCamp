e52d81f54a7fe61169d13232344060a24681ee6e
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-06-19 17:05:42 +0200

    refactor: remove unused function

--- middleware.js
+++ middleware.js
@@ -61,15 +61,3 @@ module.exports.validateReview = (req, res, next) => {
         next()
     }
 }
-
-const validateUser = (req, res, next) => {
-    const { error } = userSchema.validate(req.body)
-
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    }
-    else {
-        next()
-    }
-}
