c909be6d592514e702fe97ccb2dd4e5877c36d35
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-06-19 17:26:02 +0200

    refactor: coding convention

--- schemas.js
+++ schemas.js
@@ -1,5 +1,5 @@
-const BaseJoi = require('joi');
-const sanitizeHtml = require('sanitize-html');
+const BaseJoi = require('joi')
+const sanitizeHtml = require('sanitize-html')
 
 const extension = (joi) => ({
     type: 'string',
@@ -12,14 +12,14 @@ const extension = (joi) => ({
             validate(value, helpers) {
                 const clean = sanitizeHtml(value, {
                     allowedTags: [],
-                    allowedAttributes: {},
-                });
+                    allowedAttributes: {}
+                })
                 if (clean !== value) return helpers.error('string.escapeHTML', { value })
-                return clean;
+                return clean
             }
         }
     }
-});
+})
 
 const Joi = BaseJoi.extend(extension)
 
@@ -27,16 +27,15 @@ module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
         title: Joi.string().required().escapeHTML(),
         price: Joi.number().required().min(0),
+        // image: Joi.string().required(),
         location: Joi.string().required().escapeHTML(),
         description: Joi.string().required().escapeHTML()
     }).required(),
     deleteImages: Joi.array()
-});
-
+})
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
         body: Joi.string().required().escapeHTML()
     }).required()
 })
-
