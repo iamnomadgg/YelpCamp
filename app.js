00be1740f1db4060bb7f4738fbb881416b2fa4fb
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-06-19 17:40:36 +0200

    refactor: add session name, add a comment httpsOnly to be activated in the future

--- app.js
+++ app.js
@@ -41,11 +41,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(sanitizeV5({ replaceWith: '_' }))
 
 const sessionConfig = {
+    name: 'session',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
