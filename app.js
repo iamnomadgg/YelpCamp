5d8a42677166e7cfa891ee2c744b98e9955dc832
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-06-20 12:10:29 +0200

    chore: move secret to the env variables

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes = require('./routes/reviews')
 const userRoutes = require('./routes/users')
 
-const dbUrl = process.env.DB_URL //'mongodb://localhost:27017/yelp-camp'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!'
 mongoose.connect(dbUrl)
 
 const dbCon = mongoose.connection
@@ -93,7 +94,7 @@ const store = MongoStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60,
     crypto: {
-        secret: 'thisshouldbeabettersecret!'
+        secret
     }
 })
 
@@ -104,7 +105,7 @@ store.on('error', function (e) {
 const sessionConfig = {
     store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
